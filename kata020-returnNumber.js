eae79c02e5e51e7a74c106a54e78418424f8c9f2
jdelacruzjose <49597618+jdelacruzjose@users.noreply.github.com>
2019-08-08 19:44:07 -0400

    Add files via upload

---
+++ kata020-returnNumber.js
@@ -0,0 +1,21 @@
+// ************************************************************************************
+// https://www.codewars.com/kata/56dec885c54a926dcd001095/solutions/javascript
+// Description:
+// Very simple, given a number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34 language - the returned string is not permitted to contain any redundant trailing whitespace: you can use dynamically allocated character strings.
+// ************************************************************************************
+
+
+//////// my solution: ////////
+function opposite(number) {
+  return(-number);
+}
+
+
+//////// the best solution: ////////
+// const opposite = number => -number;
