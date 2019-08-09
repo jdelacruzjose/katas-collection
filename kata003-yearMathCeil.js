aef6767a7af0f47c2f1f104aa1bbb84569dd7148
jdelacruzjose <49597618+jdelacruzjose@users.noreply.github.com>
2019-08-09 16:51:12 -0400

    Update kata003-yearMathCeil.js

--- kata003-yearMathCeil.js
+++ kata003-yearMathCeil.js
@@ -1,5 +1,6 @@
 // ************************************************************************************
 // Description:
+// https://www.codewars.com/kata/5a3fe3dde1ce0e8ed6000097
 // Introduction
 // The first century spans from the year 1 up to and including the year 100, The second - from the year 101 up to and including the year 200, etc.
 
