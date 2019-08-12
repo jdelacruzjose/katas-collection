ea0eacfc78a50539e191f9791ad7677e199414e8
jdelacruzjose <49597618+jdelacruzjose@users.noreply.github.com>
2019-08-12 19:58:14 -0400

    Update kata001-stringSplit.js

--- kata001-stringSplit.js
+++ kata001-stringSplit.js
@@ -1,5 +1,5 @@
 // ************************************************************************************
-// https://www.codewars.com/kata/credit-card-mask/solutions?show-solutions=1
+// https://www.codewars.com/kata/credit-card-mask
 // Description:
 // Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
 
