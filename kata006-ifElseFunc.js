545f70f667ec0d96bcc97b4b6cb4f978f39aaaed
jdelacruzjose <49597618+jdelacruzjose@users.noreply.github.com>
2019-08-16 20:27:47 -0400

    Update kata006-ifElseFunc.js

--- kata006-ifElseFunc.js
+++ kata006-ifElseFunc.js
@@ -1,4 +1,5 @@
 // ************************************************************************************
+// https://www.codewars.com/kata/56606694ec01347ce800001b
 // Description:
 // Implement a method that accepts 3 integer values a, b, c. The method should return true if a triangle can be built with the sides of given length and false in any other case.
 
