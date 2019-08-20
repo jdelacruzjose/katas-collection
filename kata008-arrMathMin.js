1484b02d14294b613c5758314adbc8a1cd2f8c0f
jdelacruzjose <49597618+jdelacruzjose@users.noreply.github.com>
2019-08-19 22:29:50 -0400

    Update kata008-arrMathMin.js

--- kata008-arrMathMin.js
+++ kata008-arrMathMin.js
@@ -1,5 +1,6 @@
 // ************************************************************************************
-// /Story
+// https://www.codewars.com/kata/the-highest-profit-wins
+// Story
 // Ben has a very simple idea to make some profit: he buys something and sells it again. Of course, this wouldn't give him any profit at all if he was simply to buy and sell it at the same price. Instead, he's going to buy it for the lowest possible price and sell it at the highest.
 
 // Task
