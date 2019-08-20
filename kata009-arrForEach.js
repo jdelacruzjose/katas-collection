0bb32bee720fb21322e67413b8eb2c9b7788ea53
Jose De La Cruz <49597618+jdelacruzjose@users.noreply.github.com>
2019-08-20 14:53:29 -0400

    Update kata009-arrForEach.js

--- kata009-arrForEach.js
+++ kata009-arrForEach.js
@@ -1,6 +1,7 @@
 // ************************************************************************************
-// Return the number (count) of vowels in the given string.
+// https://www.codewars.com/kata/vowel-count
 
+// Description: Return the number (count) of vowels in the given string.
 // We will consider a, e, i, o, and u as vowels for this Kata.
 
 // The input string will only consist of lower case letters and/or spaces.
