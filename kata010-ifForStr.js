611a150bc41b5084d4370157bca1d2b0e0c0c96c
Jose De La Cruz <49597618+jdelacruzjose@users.noreply.github.com>
2019-08-21 20:30:42 -0400

    Update kata010-ifForStr.js

--- kata010-ifForStr.js
+++ kata010-ifForStr.js
@@ -1,5 +1,6 @@
 // ************************************************************************************
-// /Jamie is a programmer, and James' girlfriend. She likes diamonds, and wants a diamond string from James. Since James doesn't know how to make this happen, he needs your help.
+// https://www.codewars.com/kata/give-me-a-diamond/java
+// Description: Jamie is a programmer, and James' girlfriend. She likes diamonds, and wants a diamond string from James. Since James doesn't know how to make this happen, he needs your help.
 
 // Task
 // You need to return a string that looks like a diamond shape when printed on the screen, using asterisk (*) characters. Trailing spaces should be removed, and every line must be terminated with a newline character (\n).
