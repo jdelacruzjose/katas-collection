a000ebf57f81da35dec598a802e3b50e9a603e6d
Jose De La Cruz <49597618+jdelacruzjose@users.noreply.github.com>
2019-08-22 20:08:54 -0400

    Update kata011-forSwitchCase.js

--- kata011-forSwitchCase.js
+++ kata011-forSwitchCase.js
@@ -1,5 +1,6 @@
 // ************************************************************************************
-// The new "Avengers" movie has just been released! There are a lot of people at the cinema box office standing in a huge line. Each of them has a single 100, 50 or 25 dollars bill. An "Avengers" ticket costs 25 dollars.
+// https://www.codewars.com/kata/vasya-clerk
+// Description: The new "Avengers" movie has just been released! There are a lot of people at the cinema box office standing in a huge line. Each of them has a single 100, 50 or 25 dollars bill. An "Avengers" ticket costs 25 dollars.
 
 // Vasya is currently working as a clerk. He wants to sell a ticket to every single person in this line.
 
