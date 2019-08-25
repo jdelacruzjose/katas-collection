64a6ed8824c09cd1fd65ad4b7f767ea5b71136b0
Jose De La Cruz <49597618+jdelacruzjose@users.noreply.github.com>
2019-08-25 19:20:17 -0400

    Update kata014-forEachArr.js

--- kata014-forEachArr.js
+++ kata014-forEachArr.js
@@ -1,5 +1,6 @@
 // ************************************************************************************
-//There is a queue for the self-checkout tills at the supermarket. Your task is write a function to calculate the total time required for all the customers to check out!
+// https://www.codewars.com/kata/the-supermarket-queue
+// Description: There is a queue for the self-checkout tills at the supermarket. Your task is write a function to calculate the total time required for all the customers to check out!
 
 // input
 // customers: an array of positive integers representing the queue. Each integer represents a customer, and its value is the amount of time they require to check out.
