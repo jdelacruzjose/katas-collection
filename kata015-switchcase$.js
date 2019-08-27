58d3d9a6f92d83891275f6129ae24bf6bcd8100b
Jose De La Cruz <49597618+jdelacruzjose@users.noreply.github.com>
2019-08-26 20:53:31 -0400

    Update kata015-switchcase$.js

--- kata015-switchcase$.js
+++ kata015-switchcase$.js
@@ -1,4 +1,5 @@
 // ************************************************************************************
+// https://www.codewars.com/kata/who-likes-it
 // Description:
 // You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
 
