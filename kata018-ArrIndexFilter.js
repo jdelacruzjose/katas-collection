050fe358b6505c4652b3cf733687c2b590e4fc88
Jose De La Cruz <49597618+jdelacruzjose@users.noreply.github.com>
2019-08-29 22:19:06 -0400

    Update kata018-ArrIndexFilter.js

--- kata018-ArrIndexFilter.js
+++ kata018-ArrIndexFilter.js
@@ -1,6 +1,7 @@
 // ************************************************************************************
 // https://www.codewars.com/kata/563cf89eb4747c5fb100001b/train/javascript
-// The museum of incredible dull things
+//
+// Description: The museum of incredible dull things
 // The museum of incredible dull things wants to get rid of some exhibitions. Miriam, the interior architect, comes up with a plan to remove the most boring exhibitions. She gives them a rating, and then removes the one with the lowest rating.
 
 // However, just as she finished rating all exhibitions, she's off to an important fair, so she asks you to write a program that tells her the ratings of the items after one removed the lowest one. Fair enough.
