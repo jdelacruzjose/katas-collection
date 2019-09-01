f2f2fb055754c2ca2aad165e56fc1e0d8dd3b163
Jose De La Cruz <49597618+jdelacruzjose@users.noreply.github.com>
2019-09-01 16:53:52 -0400

    Update kata022-doWhileLoop.js

--- kata022-doWhileLoop.js
+++ kata022-doWhileLoop.js
@@ -1,5 +1,5 @@
 // ************************************************************************************
-// // https://www.codewars.com/kata/563f037412e5ada593000114/train/javascript// Description:
+// https://www.codewars.com/kata/563f037412e5ada593000114/train/javascript// Description:
 // Description: Mr. Scrooge has a sum of money 'P' that wants to invest, and he wants to know how many years 'Y' this sum has to be kept in the bank in order for this sum of money to amount to 'D'.
 
 // The sum is kept for 'Y' years in the bank where interest 'I' is paid yearly, and the new sum is re-invested yearly after paying tax 'T'
