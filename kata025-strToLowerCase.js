f752d7845ad41dab59cc894c5201cda4c9e62b23
Jose De La Cruz <49597618+jdelacruzjose@users.noreply.github.com>
2019-09-06 14:33:12 -0400

    Add files via upload

---
+++ kata025-strToLowerCase.js
@@ -0,0 +1,22 @@
+// ************************************************************************************
+// https://www.codewars.com/kata/58dbdccee5ee8fa2f9000058
+// Description: Given a string of arbitrary length with any ascii characters. 
+// Write a function to determine whether the string contains the whole word "English".
+
+// The order of characters is important -- a string "abcEnglishdef" is correct but "abcnEglishsef" is not correct.
+
+// Upper or lower case letter does not matter -- "eNglisH" is also correct.
+
+// Return value as boolean values, true for the string to contains "English", false for it does not.
+// ************************************************************************************
+
+
+//////// my solution: ////////
+function spEng(sentence){
+  let sentenceLower = sentence.toLowerCase();
+  return sentenceLower.includes('english');
+}
+
+
+//////// the best solution: ////////
+// var spEng = (sentence) => sentence.toLowerCase().includes('english');
