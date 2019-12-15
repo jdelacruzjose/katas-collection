8e5c22e0a4a534f7f946ea141651978534e11cc3
jdelacruzjose <jdelacruzjose@gmail.com>
2019-12-14 19:28:53 -0500

    latest kata

---
+++ kata042 -strUpperCase.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/makeuppercase/train/javascript
+// Description: Write function makeUpperCase.
+
+function makeUpperCase(str) {
+  return str.toUpperCase();
+}
+
+// steps
+// 1. Return the placeholder using toUpperCase method.
